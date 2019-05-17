71f29907e927697a911e62733dc157767e26bd11
Shandra Gordon <Shandra@cprsavers.local>
2019-05-17 13:23:26 -0700

    Removes fully received variable from warehouse log, as the PO has technically not yet been received in MOM at the time that warehouse sends the log out

--- src/controllers/warehouseController.js
+++ src/controllers/warehouseController.js
@@ -19,7 +19,7 @@ function warehouseController() {
         Promise.all(poPromises).then(poResponse => {
             debug(poResponse);
             poInfo = poResponse.map(po => {
-                return {'ponumber': po.ponumber, 'Supplier': po.name, 'Order Date': po.odr_date.toString().substr(4,11), 'Reference': po.reference, 'Fully Received': po.completed == 1 ? 'Yes' : 'No'};
+                return {'ponumber': po.ponumber, 'Supplier': po.name, 'Order Date': po.odr_date.toString().substr(4,11), 'Reference': po.reference};
             });
 
             if(recipients && recipients !== '') {
